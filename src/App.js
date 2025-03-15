f095133e4795c6b95f54b466854473b791ed0366
gabriel <gabrielvqueiroz@gmail.com>
2025-03-14 23:57:23 -0400

    sibebar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import './App.css';
-import logo from './logo.svg';
+import Sidebar from './components/sidebar'; 
 
 function App() {
+
   return (
     <div className="App">
-      <div className='sidebar'>
-        <img src={logo} alt='pj'/>
-        <img src={logo} alt='npc'/>
-        <img src={logo} alt='monstros'/>
-        <img src={logo} alt='anotacao'/>
-
-        <img src={logo} alt='deslogar'/>
-      </div>
+      <Sidebar/>
 
       <div className='conteudo'>chat, no react quero fazer um item suspenso com o fundo da tela ghostwhite e o item com o fundo white</div>
     </div>
---
+++ src/components/sidebar/index.js
@@ -0,0 +1,49 @@
+import './sidebar.css';
+import { useState } from 'react';
+import config from '../../res/config.svg';
+import personagem from '../../res/personagem.svg';
+import inc from '../../res/inc.svg';
+import home from '../../res/home.svg';
+import caracteristica from '../../res/caracteristica.svg';
+import logout from '../../res/logout.svg';
+import monstro from '../../res/monstro.svg';
+
+function Sidebar() {
+
+  const [menu, setMenu] = useState(1);  
+
+  return (
+    
+    <div className='sidebar'>
+      <ul className='sd-div-top'>
+        <li key='sdbt1' className={menu===1?'sd-div-item-selecionado': 'sd-div-item'} >
+          <img src={home} alt='home' onClick={()=>{setMenu(1)}}/>
+        </li>
+        <li key='sdbt2' className={menu===2?'sd-div-item-selecionado': 'sd-div-item'} >
+          <img src={personagem} alt='pj' onClick={()=>{setMenu(2)}}/>
+        </li>
+        <li key='sdbt3' className={menu===3?'sd-div-item-selecionado': 'sd-div-item'} >
+          <img src={inc} alt='npc' onClick={()=>{setMenu(3)}}/>
+        </li>
+        <li key='sdbt4' className={menu===4?'sd-div-item-selecionado': 'sd-div-item'} >
+          <img src={monstro} alt='monstros' onClick={()=>{setMenu(4)}}/>
+        </li>
+        <li key='sdbt5' className={menu===5?'sd-div-item-selecionado': 'sd-div-item'} >
+          <img src={caracteristica} alt='anotacao' onClick={()=>{setMenu(5)}}/>
+        </li>
+      </ul>
+
+      <ul className='siv-div-bottom'>
+        <li key='sdbb1' className={menu===6?'sd-div-item-selecionado': 'sd-div-item'} >
+          <img src={config} alt='perfil' onClick={()=>{setMenu(6)}}/>
+        </li>
+        <li key='sdbb2' className={menu===7?'sd-div-item-selecionado': 'sd-div-item'} >
+          <img src={logout} alt='deslogar' onClick={()=>{setMenu(7)}}/>
+        </li>
+      </ul>        
+    </div>
+
+  );
+}
+
+export default Sidebar;
