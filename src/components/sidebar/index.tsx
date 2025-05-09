003a0ab3c47c8453be834387f3f75dc43048379d
gabriel <gabrielvqueiroz@gmail.com>
2025-05-09 18:37:43 -0400

    ajustes no sidebar para reconhecero o link da pagina

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -2,7 +2,6 @@
 
 import './sidebar.css';
 import { memo } from "react";
-import { useState } from 'react';
 import logo from '@/res/logo.svg';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -11,52 +10,55 @@ import { GiCardBurn } from 'react-icons/gi';
 import { CiSettings } from 'react-icons/ci';
 import { BiLogOutCircle, BiSolidHome } from 'react-icons/bi';
 import { FaNoteSticky } from "react-icons/fa6";
+import { usePathname } from 'next/navigation';
 
 function Sidebar() {
 
-  const [menu, setMenu] = useState<number>(1); 
+  const pathName = usePathname();
 
   return(
-    <nav className='sd-container'>
+    <aside className='sd-container'>
+
       <Image className='sd_logo'src={logo} alt='RPGMestre'/>
+
       <ul className='sd-div-top'>
-        <li key='sdbt1' className={menu===1?'sd-div-item-selecionado': 'sd-div-item'} >
+        <li key='sdbt1' className={pathName === '/'?'sd-div-item-selecionado': 'sd-div-item'} >
           <Link href='/'> 
-            <BiSolidHome size={45} color='#fff' className='sd-container-img' onClick={()=>{setMenu(1)}}/>
+            <BiSolidHome size={45} color='#fff' className='sd-container-img'/>
           </Link>
         </li>
 
-        <li key='sdbt2' className={menu===2?'sd-div-item-selecionado': 'sd-div-item'} >
+        <li key='sdbt2' className={pathName === '/personagens'?'sd-div-item-selecionado': 'sd-div-item'} >
           <Link href='/personagens'> 
-            <IoPersonSharp size={45} color='#fff' className='sd-container-img' onClick={()=>{setMenu(2)}}/>
+            <IoPersonSharp size={45} color='#fff' className='sd-container-img'/>
           </Link>
         </li>
 
-        <li key='sdbt3' className={menu===3?'sd-div-item-selecionado': 'sd-div-item'} >
+        <li key='sdbt3' className={pathName === '/cartas'?'sd-div-item-selecionado': 'sd-div-item'} >
           <Link href='/cartas'> 
-            <GiCardBurn size={45} color='#fff' className='sd-container-img' onClick={()=>{setMenu(3)}}/>
+            <GiCardBurn size={45} color='#fff' className='sd-container-img'/>
           </Link>
         </li>
 
-        <li key='sdbt4' className={menu===4?'sd-div-item-selecionado': 'sd-div-item'} >
+        <li key='sdbt4' className={pathName === '/anotacoes'?'sd-div-item-selecionado': 'sd-div-item'} >
           <Link href='/anotacoes'> 
-            <FaNoteSticky size={45} color='#fff' className='sd-container-img' onClick={()=>{setMenu(4)}}/>
+            <FaNoteSticky size={45} color='#fff' className='sd-container-img'/>
           </Link>
         </li>
 
       </ul>
 
       <ul className='siv-div-bottom'>
-        <li key='sdbb1' className={menu===5?'sd-div-item-selecionado': 'sd-div-item'} >
+        <li key='sdbb1' className={pathName === '/perfil'?'sd-div-item-selecionado': 'sd-div-item'} >
           <Link href='/perfil'> 
-            <CiSettings size={45} color='#fff' className='sd-container-img' onClick={()=>{setMenu(5)}}/>
+            <CiSettings size={45} color='#fff' className='sd-container-img'/>
           </Link>
         </li>
-        <li key='sdbb2' className={menu===6?'sd-div-item-selecionado': 'sd-div-item'} >
-          <BiLogOutCircle size={45} color='#fff'  className='sd-container-img' onClick={()=>{setMenu(6)}}/>
+        <li key='sdbb2' className={'sd-div-item'} >
+          <BiLogOutCircle size={45} color='#fff'  className='sd-container-img'/>
         </li>
       </ul>     
-    </nav>
+    </aside>
   )
 }
 
